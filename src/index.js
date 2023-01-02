1b459f4b8fde281608d8aefe568adc1f325ae55f
shahanJP <113164178+shahanJP@users.noreply.github.com>
2023-01-02 16:57:39 +1100

    basename

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import { HashRouter } from "react-router-dom";
 ReactDOM.render(
-  <HashRouter basename={process.env.PUBLIC_URL} >
+  <HashRouter basename={window.location.pathname || ''} >
     <App />
     </HashRouter>,
   document.getElementById("root")
