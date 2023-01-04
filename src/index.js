1dde55de993b3adc5461556a4b7b2d79089d0876
shahanJP <113164178+shahanJP@users.noreply.github.com>
2023-01-04 15:32:47 +1100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import "./index.css";
 import App from "./App";
 import { HashRouter } from "react-router-dom";
 ReactDOM.render(
-  <HashRouter basename={process.env.PUBLIC_URL} >
+  <HashRouter  >
     <App />
     </HashRouter>,
   document.getElementById("root")
